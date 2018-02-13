d5223a9a04d194ba8aa9811e7f04b20da1150def
jannson <gandancing@gmail.com>
2018-02-13 15:37:43 +0800

    use base url

--- app.js
+++ app.js
@@ -8,18 +8,19 @@
   window.closePopup=function(){
     var div=document.getElementById('$$popup');
     div.parentNode.removeChild(div);
-    localStorage.popupClosed3="1";
+    localStorage.popupClosed = "1";
   };
   div.id = "$$popup";
   div.style.cssText = 'max-width:500px;position:fixed;bottom:0;right:0;-moz-border-radius:100px;-moz-box-shadow: 0px 0px 8px #fff;display:block;cursor:pointer;';
-  div.onclick=window.closePopup;
+  //div.onclick=window.closePopup;
   img.src=baseUrl + "koolshare-new-year-2018.png";
   img.style.cssText = 'width:100%;';
   close.style.cssText = 'width:25px;height:25px;position:absolute;top:0;right:0;margin-right:8px;'
   close.src=baseUrl + "if_misc-_close__1276877.png";
+  close.onclick=window.closePopup;
   div.appendChild(img);
   div.appendChild(close);
-  if(Number(localStorage.popupClosed3)!==1){
+  if(Number(localStorage.popupClosed)!==1){
     document.body.appendChild(div);
   }
 } )(window);

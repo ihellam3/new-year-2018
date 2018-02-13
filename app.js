0dd1b84c24fdeacdc31af0dfdf47217d3af7e3fd
jannson <gandancing@gmail.com>
2018-02-13 15:32:25 +0800

    use base url

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 (function(window) {
 	"use strict";
+  var baseUrl = "https://ddnsto.b0.upaiyun.com/new-year-2018/";
+  //var baseUrl = "./";
   var div = document.createElement('div');
   var img = document.createElement('img');
   var close = document.createElement('img');
@@ -11,10 +13,10 @@
   div.id = "$$popup";
   div.style.cssText = 'max-width:500px;position:fixed;bottom:0;right:0;-moz-border-radius:100px;-moz-box-shadow: 0px 0px 8px #fff;display:block;cursor:pointer;';
   div.onclick=window.closePopup;
-  img.src="./image.png";
+  img.src=baseUrl + "koolshare-new-year-2018.png";
   img.style.cssText = 'width:100%;';
   close.style.cssText = 'width:25px;height:25px;position:absolute;top:0;right:0;margin-right:8px;'
-  close.src="./if_misc-_close__1276877.png";
+  close.src=baseUrl + "if_misc-_close__1276877.png";
   div.appendChild(img);
   div.appendChild(close);
   if(Number(localStorage.popupClosed3)!==1){
